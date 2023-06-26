5f487b8d69f87468bce872a2045e83822a387190
Artem <tuchin12345678artem@gmail.com>
2023-06-26 13:08:34 +0300

    Добавлены переключатели страниц в таблице

--- src/main/resources/static/table.js
+++ src/main/resources/static/table.js
@@ -4,12 +4,24 @@ rows.splice(0,1);
 console.log(rows);
 let next = document.getElementById("next");
 let back = document.getElementById("back");
+let pointer = document.getElementById("page-pointer");
+let th = document.querySelectorAll('table th');
+document.getElementById('table-div').style.width=th.length*100 + 'px';
+let page_number = 0;
 let begin = 0;
 let end = 10;
 if (rows.length>10) {
 // скроем все строки после 10
-    rows.forEach((e, i) => e.style.display = i > 10 ? 'none' : 'table-row');
+    rows.forEach((e, i) => e.style.display = i > 9 ? 'none' : 'table-row');
     back.disabled = true;
+    const pages = Math.ceil(rows.length/10);
+    pointer.style.width=(pages+1)*20 + 'px';
+    for (let i = 0;i<pages; i++) {
+        let point = document.createElement('div');
+        point.className = 'point';
+        if (i===0) point.className = 'current_page';
+        pointer.appendChild(point);
+    }
 } else {
     document.getElementById("btn_container").style.display="none";
 }
@@ -18,6 +30,9 @@ function next10() {
     back.disabled = false;
     begin+=10;
     end+=10;
+    pointer.children.item(page_number).className = 'point';
+    page_number += 1;
+    pointer.children.item(page_number).className = 'current_page';
     if (end >= rows.length) next.disabled=true;
     // перебираем все строки
     rows.forEach((el, i) => {
@@ -31,6 +46,9 @@ function back10() {
     next.disabled=false;
     begin-=10;
     end-=10;
+    pointer.children.item(page_number).className = 'point';
+    page_number -= 1;
+    pointer.children.item(page_number).className = 'current_page';
     if (begin === 0) back.disabled=true;
     // перебираем все строки
     rows.forEach((el, i) => {
